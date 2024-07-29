14b7c6cda26809166f88499202a35ed282bddff0
dev <d.eugeniy@gmail.com>
2024-07-29 15:07:44 +0300

    Initial commit

---
+++ src/main/java/host/remote/controlcenter/ControlcenterApplication.java
@@ -0,0 +1,13 @@
+package host.remote.controlcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlcenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/host/remote/controlcenter/ControlcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package host.remote.controlcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
